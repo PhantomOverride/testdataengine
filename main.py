8ebfe86d9bc02a95645f0e90b08ab75170f27850
Oscar Hjelm <oscarh@elysium.local>
2019-05-17 14:45:06 +0200

    Minor tweaking

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import argparse
 from testdatagenerator import PersonalNumberGenerator
 
+
 def main():
-    parser = argparse.ArgumentParser(description="A tool for generating relevant testdata for testing.")
-    parser.add_argument('--pnr', action='store_true', default=False, help="Generate Personal Number") 
+    parser = argparse.ArgumentParser(description="A tool for generating input data. Use stand-alone or in an existing workflow to supply data.")
+    parser.add_argument('-p', '--pnr', action='store_true', default=False, help="Generate Personal Number")
     args = parser.parse_args()
     
     if args.pnr:
         print(PersonalNumberGenerator.pnr())
     else:
-        print("test")
-
-
+        print("No options supplied!")
 
 
 if __name__ == "__main__":
--- testdatagenerator/PersonalNumberGenerator.py
+++ testdatagenerator/PersonalNumberGenerator.py
@@ -2,9 +2,10 @@ from testdatagenerator.Utilities import luhn
 from faker import Faker
 from random import randint
 
+
 def pnr():
     generator = Faker()
     date = generator.date_time()
     pnr = date.strftime("%y%m%d-")
-    pnr += str(randint(0,9)) + str(randint(0,9)) + str(randint(0,9))
+    pnr += str(randint(0, 9)) + str(randint(0, 9)) + str(randint(0, 9))
     return pnr + str(luhn(pnr))
