03b0c52a882a1517dcc99c71a86b7a27ff6477a3
Oscar Hjelm <oscarh@elysium.local>
2019-05-17 15:02:03 +0200

    Added name generation

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import argparse
-from testdatagenerator import PersonalNumberGenerator
+from testdatagenerator import PersonalNumberGenerator, PersonNameGenerator
+
 
 
 def main():
     parser = argparse.ArgumentParser(description="A tool for generating input data. Use stand-alone or in an existing workflow to supply data.")
     parser.add_argument('-p', '--pnr', action='store_true', default=False, help="Generate Personal Number")
+    parser.add_argument('-n', '--name', action='store_true', default=False, help="Generate Personal Number")
     args = parser.parse_args()
     
     if args.pnr:
         print(PersonalNumberGenerator.pnr())
+    elif args.name:
+        p = PersonNameGenerator.PersonNameGenerator()
+        print(p.full_name())
     else:
         print("No options supplied!")
 
